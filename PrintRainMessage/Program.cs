9a25a8103c452b0d4121728b918595ec56a5c4f8
sabbir-ul-alam <sabbir.ul.alam.sabbir@gmail.com>
2021-08-06 20:52:29 +0600

    use console.write to print

---
+++ PrintRainMessage/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrintRainMessage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, world");
+            Console.WriteLine("Chinese Democracy is done and it's November");
+            Console.WriteLine("Is it raining?");
+            Console.WriteLine();
+        }
+    }
+}
