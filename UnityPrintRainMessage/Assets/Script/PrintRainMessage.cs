13311e5a8c673330d3a08d340431c8dda9ae5799
sabbir-ul-alam <sabbir.ul.alam.sabbir@gmail.com>
2021-08-07 02:04:05 +0600

    Debug Log print in unity

---
+++ UnityPrintRainMessage/Assets/Script/PrintRainMessage.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PrintRainMessage : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log ("Hello, world");
+        Debug.Log("Chinese Democracy is done and it's November");
+        Debug.Log("Is it raining?");
+
+    }
+
+    // Update is called once per frame
+   
+}
