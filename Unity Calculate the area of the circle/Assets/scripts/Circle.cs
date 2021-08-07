da70b5cc3df00ef99b279215afa69eeeada39bca
sabbir-ul-alam <sabbir.ul.alam.sabbir@gmail.com>
2021-08-08 01:02:40 +0600

    Created new empty game object
    added sprites circle and scripts to it.
    changes the scale based on radius.

---
+++ Unity Calculate the area of the circle/Assets/scripts/Circle.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Circle : MonoBehaviour
+{
+    [SerializeField]
+    int radius;
+    float area;
+    // Start is called before the first frame update
+    void Start()
+    {
+        area = Mathf.PI * Mathf.Pow(radius, 2);
+        // scale circle sprite based on radius 
+        Vector3 scale = transform.localScale;
+        scale.x *= radius;
+        scale.y *= radius;
+        transform.localScale = scale;
+        print("Radius: " + radius + ", Area: " + area);
+
+
+
+    }
+
+ 
+}
