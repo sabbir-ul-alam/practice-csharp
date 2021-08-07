a94e6ab44ee969b5931212b236301be6d0d02539
sabbir-ul-alam <sabbir.ul.alam.sabbir@gmail.com>
2021-08-07 16:11:29 +0600

    print area of a circle with number formate upto 2 decimal point

---
+++ Calculate the area of the circles/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculate_the_area_of_the_circles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int radius;
+            float area;
+            const float PI = (float)Math.PI;
+            for (radius = 0; radius <= 5; radius++)
+            {
+                area = PI * (float)Math.Pow(radius, 2);
+                Console.WriteLine("Radius: {0},  Area: {1,5:N2}",radius,area);
+            }
+        }
+    }
+}
