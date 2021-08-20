8286789cad4643de5ddc33fc073c738e236bee07
sabbir-ul-alam <sabbir.ul.alam.sabbir@gmail.com>
2021-08-21 02:02:24 +0600

    Added teddy bear object Added vecloticy/force Removed Gravity from projectr setting

---
+++ FishiEatingBear/Assets/Scripts/TeadyBearClass.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeadyBearClass : MonoBehaviour
+{
+    [SerializeField]
+    float minImpulseForce;
+    [SerializeField]
+    float maxImpulseForce;
+    // Start is called before the first frame update
+    void Start()
+    {
+        float angle = Random.Range(0, 2 * Mathf.PI);
+        Vector2 direction = new Vector2(
+            Mathf.Cos(angle), Mathf.Sin(angle));
+        float magnitude = Random.Range(minImpulseForce, maxImpulseForce);
+        GetComponent<Rigidbody2D>().AddForce(
+            direction * magnitude,
+            ForceMode2D.Impulse);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
