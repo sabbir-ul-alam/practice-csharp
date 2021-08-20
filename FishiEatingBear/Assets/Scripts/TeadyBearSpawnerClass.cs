3bfb580143c46afefd76d7269111de9a4cb08dd2
sabbir-ul-alam <sabbir.ul.alam.sabbir@gmail.com>
2021-08-21 03:09:04 +0600

    Added teddy spawner class

--- FishiEatingBear/Assets/Scripts/TeadyBearSpawnerClass.cs
+++ FishiEatingBear/Assets/Scripts/TeadyBearSpawnerClass.cs
@@ -2,17 +2,82 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TeadyBearSpawnerClass : MonoBehaviour
+/// <summary>
+/// A teddy bear spawner
+/// </summary>
+public class TeddyBearSpawner : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+	// needed for spawning
+	[SerializeField]
+	GameObject prefabTeddyBear;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	// saved for efficiency
+	[SerializeField]
+	Sprite teddyBearSprite0;
+
+	// spawn control
+	const float MinSpawnDelay = 1;
+	const float MaxSpawnDelay = 2;
+	Timer spawnTimer;
+
+	// spawn location support
+	const int SpawnBorderSize = 100;
+	int minSpawnX;
+	int maxSpawnX;
+	int minSpawnY;
+	int maxSpawnY;
+
+
+	/// <summary>
+	/// Use this for initialization
+	/// </summary>
+	void Start()
+	{
+		// save spawn boundaries for efficiency
+		minSpawnX = SpawnBorderSize;
+		maxSpawnX = Screen.width - SpawnBorderSize;
+		minSpawnY = SpawnBorderSize;
+		maxSpawnY = Screen.height - SpawnBorderSize;
+
+		// create and start timer
+		spawnTimer = gameObject.AddComponent<Timer>();
+		spawnTimer.Duration = Random.Range(MinSpawnDelay, MaxSpawnDelay);
+		spawnTimer.Run();
+	}
+
+	/// <summary>
+	/// Update is called once per frame 
+	/// </summary>
+	void Update()
+	{
+		// check for time to spawn a new teddy bear
+		if (spawnTimer.Finished)
+		{
+			SpawnBear();
+
+			// change spawn timer duration and restart
+			spawnTimer.Duration = Random.Range(MinSpawnDelay, MaxSpawnDelay);
+			spawnTimer.Run();
+		}
+	}
+
+	/// <summary>
+	/// Spawns a new teddy bear at a random location
+	/// </summary>
+	void SpawnBear()
+	{
+		// generate random location and create new teddy bear
+		Vector3 location = new Vector3(Random.Range(minSpawnX, maxSpawnX),
+										Random.Range(minSpawnY, maxSpawnY),
+										-Camera.main.transform.position.z);
+		Vector3 worldLocation = Camera.main.ScreenToWorldPoint(location);
+		GameObject teddyBear = Instantiate(prefabTeddyBear) as GameObject;
+		teddyBear.transform.position = worldLocation;
+
+		// set random sprite for new teddy bear
+		SpriteRenderer spriteRenderer = teddyBear.GetComponent<SpriteRenderer>();
+
+		spriteRenderer.sprite = teddyBearSprite0;
+
+	}
 }
